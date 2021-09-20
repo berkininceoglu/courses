f412c1d8b9c47f018c27d65c56a0a3da4a03d63a
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-09-20 21:18:39 +0300

    add quizzler skeleton project

---
+++ Quizzler/Quizzler-iOS13/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = "Is two plus two equals four?"
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        print("answered")
+    }
+    
+}
+
