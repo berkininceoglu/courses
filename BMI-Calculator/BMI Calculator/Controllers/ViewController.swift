3dc0e696a9a3afbbc07a5f9366c44049bb48b829
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-09-22 21:57:28 +0300

    (challenge) add slider listener to change label

--- BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightSlider: UIStackView!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightSlider: UIStackView!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        if(sender.tag == 1){
+            heightLabel.text = NSString(format: "%.2f", sender.value) as String
+        }else{
+            weightLabel.text = NSString(format: "%.0f", sender.value) as String
+        }
+    }
+    
 }
 
