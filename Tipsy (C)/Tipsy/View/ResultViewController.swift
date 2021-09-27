a17e578218bbbc7f70e245b82d646cde809b7847
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-09-27 22:41:35 +0300

    split viewcontroller into two as calculator and result -tipsy

---
+++ Tipsy (C)/Tipsy/View/ResultViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Berkin İnceoğlu on 27.09.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var splittedValueField: UILabel!
+    @IBOutlet weak var splitMessageField: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
