5b6d9aca060115774c708e4b55b0fa6b5105bf46
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-09-27 21:46:50 +0300

    add basic functionalities -tipsy

--- Tipsy (C)/Tipsy/ViewController.swift
+++ Tipsy (C)/Tipsy/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var billTextFiled: UITextField!
+    @IBOutlet weak var splitField: UILabel!
+    
+    var tip : Int = 0
+    
+    @IBAction func zeroTipPressed(_ sender: UIButton) {
+        setTipValue(p: 0)
+    }
+    @IBAction func tenTipPressed(_ sender: UIButton) {
+        setTipValue(p: 10)
+    }
+    @IBAction func twentyTipPressed(_ sender: Any) {
+        setTipValue(p: 20)
+    }
+    
+    @IBAction func splitCountChanged(_ sender: UIStepper) {
+        let splitCount = Int(sender.value)
+        splitField.text = "\(splitCount)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func setTipValue(p percentage: Int) {
+        tip = percentage
+        print(tip)
+    }
 
 }
 
