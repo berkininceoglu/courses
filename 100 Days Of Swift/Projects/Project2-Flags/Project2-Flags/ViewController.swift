ad4ecaff3aacb4c8d755c17d77834e3dfc1a70ca
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-10-02 00:08:44 +0300

    init project 2 - day 19 #100daysofswift

---
+++ 100 Days Of Swift/Projects/Project2-Flags/Project2-Flags/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2-Flags
+//
+//  Created by Berkin İnceoğlu on 1.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
