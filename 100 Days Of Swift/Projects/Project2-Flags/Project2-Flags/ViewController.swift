25cb98c854ec98a71d858697a79fae86184d8dfa
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-10-02 17:35:14 +0300

    update flag views - day 19 #100daysofswift

--- 100 Days Of Swift/Projects/Project2-Flags/Project2-Flags/ViewController.swift
+++ 100 Days Of Swift/Projects/Project2-Flags/Project2-Flags/ViewController.swift
@@ -12,11 +12,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var flag2: UIButton!
     @IBOutlet weak var flag3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+countries += ["estonia", "france", "germany", "italy", "ireland", "monaco", "nigeria", "poland", "russia", "uk", "us"]
+        
+        askQuestion()
+        
+        flag1.layer.borderWidth = 1
+        flag2.layer.borderWidth = 1
+        flag3.layer.borderWidth = 1
+        
+        flag1.layer.borderColor = UIColor.lightGray.cgColor
+        flag2.layer.borderColor = UIColor.lightGray.cgColor
+        flag3.layer.borderColor = UIColor.lightGray.cgColor
+        
     }
-
+    
+    func askQuestion(){
+        flag1.setImage(UIImage(named: countries[0]), for: .normal)
+        
+        flag2.setImage(UIImage(named: countries[1]), for: .normal)
+        
+        flag3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 
