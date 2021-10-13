aaf240027f61caddf95c63e3ae7f879511e57672
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-10-14 00:15:12 +0300

    add error messages for wrong words day 28

--- 100 Days Of Swift/Projects/Project5-WordScramble/Project5-WordScramble/ViewController.swift
+++ 100 Days Of Swift/Projects/Project5-WordScramble/Project5-WordScramble/ViewController.swift
@@ -62,17 +62,38 @@ class ViewController: UITableViewController {
     func submit(_ answer: String){
         let lowerAnswer = answer.lowercased()
         
+        let errorTitle : String
+        let errorMessage : String
+        
         if isPossible(word: lowerAnswer){
             if isOriginal(word: lowerAnswer){
                 if isReal(word: lowerAnswer){
-                    
+        
                     usedWords.insert(answer, at: 0)
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                }
+                else{
+                    errorTitle = "Word is not recognized!"
+                    errorMessage = "You cannot just make words up."
                 }
             }
+            else{
+                errorTitle = "Word already used!"
+                errorMessage = "Be more original."
+                
+            }
+        }else{
+            errorTitle = "Word is not possible!"
+            errorMessage = "You cannot spell that word from \(title!.lowercased())."
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .cancel))
+        present(ac, animated: true)
     }
     
     func isPossible(word : String) -> Bool{
