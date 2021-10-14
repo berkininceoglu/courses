146791d8695a8ee5021da31be3641b7c300feacf
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-10-14 23:55:07 +0300

    fix bug due to case sensitive situation day29

--- 100 Days Of Swift/Projects/Project5-WordScramble/Project5-WordScramble/ViewController.swift
+++ 100 Days Of Swift/Projects/Project5-WordScramble/Project5-WordScramble/ViewController.swift
@@ -74,7 +74,7 @@ class ViewController: UITableViewController {
             if isOriginal(word: lowerAnswer){
                 if isReal(word: lowerAnswer){
         
-                    usedWords.insert(answer, at: 0)
+                    usedWords.insert(lowerAnswer, at: 0)
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
