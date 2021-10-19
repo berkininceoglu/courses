e47f1de968015c4ae0246e942b25772374a91a29
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-10-19 23:16:37 +0300

    init project 7
    add tab controller
    add petition models
    add json parser

---
+++ 100 Days Of Swift/Projects/Project7-JSON/Project7-JSON/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7-JSON
+//
+//  Created by Berkin İnceoğlu on 19.10.2021.
+//
+
+import Foundation
+
+struct Petitions : Codable {
+    var results: [Petition]
+}
