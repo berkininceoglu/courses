162cbeef76d4935195103ca122f46f873fb7163b
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-10-21 13:43:45 +0300

    update html templdate day35

--- 100 Days Of Swift/Projects/Project7-JSON/Project7-JSON/DetailViewController.swift
+++ 100 Days Of Swift/Projects/Project7-JSON/Project7-JSON/DetailViewController.swift
@@ -22,14 +22,34 @@ class DetailViewController: UIViewController {
         guard let detailItem = detailItem else { return }
         
         let html = """
-        <html>
-        <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style> body { font-size: 150%; } </style>
-        </head>
-        <body>
-        \(detailItem.body)
-        </body>
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8" />
+                <style type="text/css">
+                    body{
+                        font-family:"Helvetica Neue UltraLight",helvetica,verdana;
+                        margin-left:0;
+                        margin-right:0;
+                    }
+                    section{
+                        padding-left:5px;
+                        padding-right:5px;
+                        font-size:45px;
+                    }
+                    h1{
+                        font-size: 65px;
+                        padding-left:5px;
+                    }
+
+                </style>
+            </head>
+            <body>
+               <h1>\(detailItem.title)</h1>
+               <section>
+                    <p>\(detailItem.body)</p>
+               </section>
+            </body>
         </html>
         """
         
