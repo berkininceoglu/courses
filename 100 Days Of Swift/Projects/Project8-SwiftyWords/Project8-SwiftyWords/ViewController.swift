85e3d2486eb763ce067da558bbb0d6c94c828774
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-10-22 00:35:03 +0300

    completed day36

--- 100 Days Of Swift/Projects/Project8-SwiftyWords/Project8-SwiftyWords/ViewController.swift
+++ 100 Days Of Swift/Projects/Project8-SwiftyWords/Project8-SwiftyWords/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
         cluesLabel.font = UIFont.systemFont(ofSize: 24)
         cluesLabel.text = "CLUES"
         cluesLabel.numberOfLines = 0
+        cluesLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
         view.addSubview(cluesLabel)
         
         answersLabel = UILabel()
@@ -38,6 +39,8 @@ class ViewController: UIViewController {
         answersLabel.textAlignment = .right
         answersLabel.text = "ANSWERS"
         answersLabel.numberOfLines = 0
+        answersLabel.setContentHuggingPriority(UILayoutPriority(1), for: .vertical)
+
         view.addSubview(answersLabel)
         
         currentAnswer = UITextField()
@@ -57,6 +60,10 @@ class ViewController: UIViewController {
         clear.translatesAutoresizingMaskIntoConstraints = false
         clear.setTitle("CLEAR", for: .normal)
         view.addSubview(clear)
+        
+        let buttonsView = UIView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(buttonsView)
         
         NSLayoutConstraint.activate(
             [
@@ -81,9 +88,32 @@ class ViewController: UIViewController {
                 
                 clear.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100),
                 clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),
-                clear.heightAnchor.constraint(equalToConstant: 44)
+                clear.heightAnchor.constraint(equalToConstant: 44),
+                
+                buttonsView.widthAnchor.constraint(equalToConstant: 750),
+                buttonsView.heightAnchor.constraint(equalToConstant: 350),
+                buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                buttonsView.topAnchor.constraint(equalTo: submit.bottomAnchor),
+                buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
             ])
         
+        let width = 150
+        let height = 80
+        
+        for row in 0..<4{
+            for column in 0..<5{
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+                letterButton.setTitle("WWWW", for: .normal)
+                
+                let frame = CGRect(x: column * width, y: row * height, width: width, height: height)
+                letterButton.frame = frame
+                
+                buttonsView
+                    .addSubview(letterButton)
+                letterButtons.append(letterButton)
+            }
+        }
         
     }
     
