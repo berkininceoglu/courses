ea15bcad536c258bf32293a54e3f01e3edb6538a
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-10-27 21:09:17 +0300

    init collectionview day42

---
+++ 100 Days Of Swift/Projects/Project10-Faces/Project10-Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10-Faces
+//
+//  Created by Berkin İnceoğlu on 27.10.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+}
