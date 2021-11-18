8af4344401fcc0135796a02cacd6a0166d80a9a3
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-11-18 23:54:46 +0300

    init project17 day62

---
+++ 100DaysOfSwift - Paul Hudson/Projects/Project17-SpaceRace/Project17-SpaceRace/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project17-SpaceRace
+//
+//  Created by Berkin İnceoğlu on 18.11.2021.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
