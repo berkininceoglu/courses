8077dedb08c4f19ab7079689e10326299b8e918a
inceogluberkin <inceogluberkin@sabanciuniv.edu>
2021-11-20 23:15:44 +0300

    add enemies and score growth day62

--- 100DaysOfSwift - Paul Hudson/Projects/Project17-SpaceRace/Project17-SpaceRace/GameScene.swift
+++ 100DaysOfSwift - Paul Hudson/Projects/Project17-SpaceRace/Project17-SpaceRace/GameScene.swift
@@ -12,6 +12,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var player: SKSpriteNode!
     var scoreLabel: SKLabelNode!
     
+    var enemies = ["ball", "hammer", "tv"]
+    var gameTimer: Timer?
+    var isGameOver = false
+    
     var score = 0 {
         didSet{
             scoreLabel.text = "Score: \(score)"
@@ -20,33 +24,61 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func didMove(to view: SKView) {
         backgroundColor = .black
-
-
+        
+        
         starfield = SKEmitterNode(fileNamed: "starfield")!
-            starfield.position = CGPoint(x: 1024, y: 384)
-            starfield.advanceSimulationTime(10)
-            addChild(starfield)
-            starfield.zPosition = -1
-
-            player = SKSpriteNode(imageNamed: "player")
-            player.position = CGPoint(x: 100, y: 384)
-            player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
-            player.physicsBody?.contactTestBitMask = 1
-            addChild(player)
-
-            scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
-            scoreLabel.position = CGPoint(x: 32, y: 48)
-            scoreLabel.horizontalAlignmentMode = .left
-            addChild(scoreLabel)
-
-            score = 0
-
-            physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-            physicsWorld.contactDelegate = self
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10)
+        addChild(starfield)
+        starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 32, y: 48)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+        
+        score = 0
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func createEnemy(){
+        guard let enemy = enemies.randomElement() else { return }
+        
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position  = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.angularDamping = 0
+        sprite.physicsBody?.linearDamping = 0
+        
         
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        for node in children{
+            if node.position.x < -300{
+                node.removeFromParent()
+            }
+            
+        }
+        if !isGameOver{
+            score += 1
+        }
+        
     }
 }
